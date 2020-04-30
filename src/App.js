45d855935d1b8a6848e46e91eea8632b38d010a6
Saloni <suparekh01@gmail.com>
2020-04-30 14:15:00 +0530

    API key and ID added

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = '0bc597ad';
+  const APP_KEY = '245a754f5b923ec2ec72916fd267222b	';
+
+  const egRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
-}
+};
+
 
 export default App;
