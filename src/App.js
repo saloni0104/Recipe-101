1ce1b0531763f7f8a7b93be9e263cdad9a2cf764
Saloni <suparekh01@gmail.com>
2020-04-30 15:49:18 +0530

    State changed and data fetched from api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
@@ -7,7 +6,17 @@ const App = () => {
   const APP_ID = '0bc597ad';
   const APP_KEY = '245a754f5b923ec2ec72916fd267222b	';
 
-  const egRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);      //second parameter makes sure the the effect produces request only once when application is mounted
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);   // got it from documentation of recipe search app from edamam.com
+    const data = await response.json();   // to format it in a way so that we can work easily
+    setRecipes(data.hits);   //hits fetched from the whole lot of data from api
+  }
 
   return(
     <div className="App">
